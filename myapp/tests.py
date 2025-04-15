d719e7d8a57d24451122a7f4075e692f2512fb64
jorgetallo <jabernal90@gmail.com>
2025-04-14 20:31:29 -0400

    Add tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class HomeTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_endpoint_home_responde_grupo3(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {"Grupo": "3"})
